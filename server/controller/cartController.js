bf58e50ff848d18702878a306bcee2a7f1cea4ea
efs0-cod3 <eflearnng@gmail.com>
2023-03-10 06:41:43 -0400

    getting better going strong

--- server/controller/cartController.js
+++ server/controller/cartController.js
@@ -1,37 +1,48 @@
-const Cart = require("../model/Cart")
-const User = require("../model/User")
-const Product = require("../model/Product")
+const Cart = require("../model/Cart");
+const User = require("../model/User");
+const Product = require("../model/Product");
 
-
-
-exports.addToCart = async (req,res) => {
-    // adds product to cart!
+exports.addToCart = async (req, res) => {
+  // adds product to cart!
   try {
-    const user = await User.findById(req.userId)
-    const cart = await Cart.findById(user.cart)
-    const product = await Product.findById({_id: req.params.id})
-    cart.products = cart.products.concat(product.id)
-    cart.save()
-    console.log({cart});
-    res.status(201).json({message: "product added to cart", cart})
+    const user = await User.findById(req.userId);
+    const cart = await Cart.findById(user.cart);
+    const product = await Product.findById({ _id: req.params.id });
+    if (cart.products.includes(product.id)) {
+      res.status(200).json({ message: "Item already exists in cart" });
+    }else{
+      cart.products = cart.products.concat(product.id);
+      cart.save();
+      res.status(200).json(cart);
+    }
   } catch (error) {
     console.log(error);
-    res.status(400).json({error})
+    res.status(400).json({ error });
   }
-}
+};
 
-exports.delProductFromCart = async (req,res) => {
-    // delete product from cart!
+exports.getCartProducts = async (req, res) => {
+  try {
+    const user = await User.findById(req.userId);
+    const cart = await Cart.findById(user.cart);
+    const cartProducts = await cart.populate("products");
+    res.status(200).json({ message: "Cart products", cart });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.delProductFromCart = async (req, res) => {
+  // delete product from cart!
   try {
     const user = await User.findById(req.userId);
     const cart = await Cart.findById(user.cart);
     // const product = await Product.findById({_id: req.params.id});
-    await cart.updateOne({$pull: {products: req.params.id}});
-    cart.count = await cart.countDocuments(cart.products)
-    cart.save()
-    res.status(201).json({message: "product added to cart", cart})
+    await cart.updateOne({ $pull: { products: req.params.id } });
+  
+    cart.save();
+    res.status(200).json({ message: "product deleted from cart", cart });
   } catch (error) {
-    console.log(error);
-    res.status(400).json({error})
+    res.status(400).json(error);
   }
-}
+};
